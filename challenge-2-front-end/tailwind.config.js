b91de61258a79106763b8c498582726cf0f594e8
Julian van Husen <210549@student.glu.nl>
2023-03-10 15:24:50 +0100

    init

---
+++ challenge-2-front-end/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    colors: {
+      // dark theme
+      white: "#ffffff",
+      "header-footer-dark": "#1F1F1F",
+      "background-light": "#E3E3E3",
+      "background-dark": "#292929",
+      "background-sidebar-dark": "#3E3E3E",
+      "background-message-dark": "#2B2B2B",
+      "text-dark": "#606060",
+      "rooms-button-dark": "#606060",
+      // light theme
+    },
+  },
+  plugins: [],
+};
+
+// #E3E3E3
